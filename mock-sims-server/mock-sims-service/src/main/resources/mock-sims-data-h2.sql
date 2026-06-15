451d88f641eeb90c4e49c63ca8290fabacc543d6
Siddarth Padmanabhan <siddarth.padmanabhan@kroger.com>
2026-06-15 09:02:32 -0400

    Added Table Schema and mock data

---
+++ mock-sims-server/mock-sims-service/src/main/resources/mock-sims-data-h2.sql
@@ -0,0 +1,44 @@
+// For Mark Down
+INSERT INTO MARKDOWN_RULES
+VALUES ('62000', 20, TRUE, 1, 2, 3);
+INSERT INTO MARKDOWN_RULES
+VALUES ('64307', 20, TRUE, 1, 2, 3);
+INSERT INTO MARKDOWN_RULES
+VALUES ('44805', 20, TRUE, 1, 2, 3);
+INSERT INTO MARKDOWN_RULES
+VALUES ('40610', 20, TRUE, 1, 2, 3);
+INSERT INTO MARKDOWN_RULES
+VALUES ('53000', 20, TRUE, 1, 2, 3);
+INSERT INTO MARKDOWN_RULES
+VALUES ('56005', 20, TRUE, 1, 2, 3);
+INSERT INTO MARKDOWN_RULES
+VALUES ('04703', 20, TRUE, 1, 2, 3);
+INSERT INTO MARKDOWN_RULES
+VALUES ('98418', 20, TRUE, 1, 2, 3);
+INSERT INTO MARKDOWN_RULES
+VALUES ('27602', 20, TRUE, 1, 2, 3);
+INSERT INTO MARKDOWN_RULES
+VALUES ('29465', 20, TRUE, 1, 2, 3);
+
+// For Product Basic Info
+INSERT INTO PRODUCT_BASIC_INFO (upc_number, subcommodity_number, department_number, product_name, standard_price)
+VALUES ('4011', '62000', '07', 'Bananas', 0.75);
+INSERT INTO PRODUCT_BASIC_INFO (upc_number, subcommodity_number, department_number, product_name, standard_price)
+VALUES ('4022', '64307', '07', 'ST Cashews', 4.99);
+INSERT INTO PRODUCT_BASIC_INFO (upc_number, subcommodity_number, department_number, product_name, standard_price)
+VALUES ('3001', '44805', '10', 'Feta Greek', 4.79);
+INSERT INTO PRODUCT_BASIC_INFO (upc_number, subcommodity_number, department_number, product_name, standard_price)
+VALUES ('3012', '40610', '10', 'Shortbread Butter Cookies', 3.79);
+INSERT INTO PRODUCT_BASIC_INFO (upc_number, subcommodity_number, department_number, product_name, standard_price)
+VALUES ('1001', '53000', '09', 'Chicken Breast Boneless', 9.48);
+INSERT INTO PRODUCT_BASIC_INFO (upc_number, subcommodity_number, department_number, product_name, standard_price)
+VALUES ('1002', '56005', '09', 'KRO GRND BF Burger', 7.49);
+INSERT INTO PRODUCT_BASIC_INFO (upc_number, subcommodity_number, department_number, product_name, standard_price)
+VALUES ('2002', '04703', '01', 'Merry Edwards Pinot Noir', 6.99);
+INSERT INTO PRODUCT_BASIC_INFO (upc_number, subcommodity_number, department_number, product_name, standard_price)
+VALUES ('2004', '98418', '01', 'LA PREF Garbanzo Chickpeas', 2.29);
+INSERT INTO PRODUCT_BASIC_INFO (upc_number, subcommodity_number, department_number, product_name, standard_price)
+VALUES ('2006', '27602', '01', 'Motts Fruit Snacks', 8.49);
+INSERT INTO PRODUCT_BASIC_INFO (upc_number, subcommodity_number, department_number, product_name, standard_price)
+VALUES ('2008', '29465', '01', 'Honey Bunches Oats', 4.29);
+
---
+++ mock-sims-server/mock-sims-service/src/main/resources/mock-sims-schema-h2.sql
@@ -0,0 +1,118 @@
+
+
+CREATE TABLE ORDER_TRANSACTION_INFO
+(
+    product_order_id int AUTO_INCREMENT PRIMARY KEY,
+    store_number varchar(5) NOT NULL,
+    division_number varchar(3) NOT NULL,
+    user_euid varchar(10),
+    order_placed_time timestamp NOT NULL,
+    order_received_time timestamp
+);
+
+CREATE TABLE MARKDOWN_RULES
+(
+    subcommodity_number varchar(15) PRIMARY KEY,
+    first_markdown_percent int,
+    can_be_marked_down boolean,
+    days_before_exp_to_markdown_number int,
+    days_before_exp_to_rfi_number int,
+    days_after_order_to_set_exp int
+);
+
+CREATE TABLE PRODUCT_BASIC_INFO
+(
+    upc_number varchar(15) PRIMARY KEY,
+    subcommodity_number varchar(5) NOT NULL,
+    department_number varchar(2) NOT NULL,
+    product_name varchar(100) NOT NULL,
+    standard_price decimal(10,2) NOT NULL,
+    CONSTRAINT FK_PRODUCT_BASIC_MARKDOWN
+        FOREIGN KEY (subcommodity_number)
+            REFERENCES MARKDOWN_RULES(subcommodity_number)
+);
+
+CREATE TABLE PRODUCT_BOH_INFO
+(
+    division_number varchar(3) NOT NULL,
+    store_number varchar(5) NOT NULL,
+    upc_number varchar(15) NOT NULL,
+    qod_number int NOT NULL,
+    qom_number int NOT NULL,
+    CONSTRAINT PK_PRODUCT_BOH_INFO
+        PRIMARY KEY (division_number, store_number, upc_number),
+    CONSTRAINT FK_PRODUCT_BOH_UPC
+        FOREIGN KEY (upc_number)
+        REFERENCES PRODUCT_BASIC_INFO(upc_number)
+
+);
+
+CREATE TABLE PRODUCT_INVENTORY_INFO
+(
+    product_order_id int PRIMARY KEY,
+    upc_number varchar(15) NOT NULL,
+    quantity int,
+    expiration_date date,
+    order_date date,
+    is_active boolean,
+    CONSTRAINT FK_PRODUCT_INVENTORY_ORDER
+        FOREIGN KEY (product_order_id)
+        REFERENCES ORDER_TRANSACTION_INFO(product_order_id),
+    CONSTRAINT FK_PRODUCT_INVENTORY_UPC
+        FOREIGN KEY (upc_number)
+        REFERENCES PRODUCT_BASIC_INFO(upc_number)
+);
+
+ALTER TABLE PRODUCT_INVENTORY_INFO
+    ADD CONSTRAINT CHECK_PRODUCT_INVENTORY_QUANTITY CHECK (quantity >= 0);
+
+CREATE TABLE PDM_ALERTS
+(
+    alert_id int AUTO_INCREMENT PRIMARY KEY,
+    store_number varchar(5) NOT NULL,
+    division_number varchar(3) NOT NULL,
+    department_number varchar(2) NOT NULL,
+    upc_number varchar(15) NOT NULL,
+    quantity int,
+    expiration_date date,
+    is_active BOOLEAN,
+    alert_actioned_time timestamp,
+    alert_actioned_user_euid varchar(10),
+    alert_actioned_code varchar(4),
+
+    CONSTRAINT FK_PDM_ALERTS_UPC
+        FOREIGN KEY (upc_number)
+        REFERENCES PRODUCT_BASIC_INFO(upc_number)
+);
+
+CREATE TABLE RFI_TRANSACTIONS (
+      transaction_id int AUTO_INCREMENT PRIMARY KEY,
+      user_euid varchar(10),
+      store_number varchar(5) NOT NULL,
+      division_number varchar(3) NOT NULL,
+      product_upc varchar(15) NOT NULL,
+      action_time timestamp,
+      quantity_removed int,
+      reason_code varchar(10),
+
+      CONSTRAINT FK_RFI_PRODUCT
+          FOREIGN KEY (product_upc)
+          REFERENCES PRODUCT_BASIC_INFO(upc_number)
+);
+
+CREATE TABLE MD_TRANSACTIONS (
+     transaction_id int AUTO_INCREMENT PRIMARY KEY,
+     user_euid varchar(10),
+     store_number varchar(5) NOT NULL,
+     division_number varchar(3) NOT NULL,
+     product_upc varchar(15) NOT NULL,
+     action_time timestamp,
+     quantity_marked_down int,
+     original_price decimal(10,2),
+     new_price decimal(10,2),
+
+    CONSTRAINT FK_MD_PRODUCT
+        FOREIGN KEY (product_upc)
+        REFERENCES PRODUCT_BASIC_INFO(upc_number)
+);
+
