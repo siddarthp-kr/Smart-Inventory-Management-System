0d4e58ec42020d4e002be0f8690b86b78dca3efb
Siddarth Padmanabhan <siddarth.padmanabhan@kroger.com>
2026-06-18 08:56:49 -0400

    login-stuff update

--- frontend/mock-sims-app/src/app/components/LoginButton.ts
+++ frontend/mock-sims-app/src/app/components/LoginButton.ts
@@ -8,11 +8,34 @@ import { AuthService } from '../services/auth';
   imports: [RouterLink],
   template: `
     @if (auth.isLoggedIn()) {
-      <button class="login-btn" (click)="handleLogout()">Logout</button>
+      <div class = "login-info">
+        <span class = "user-label">
+          Logged in as: {{auth.user()?.userEuid}}
+        </span>
+        <button class="login-btn" (click)="handleLogout()">Logout</button>
+      </div>
     } @else {
       <button class="login-btn" routerLink="/LoginPage">Login</button>
     }
   `,
+  styles: [`
+    .login-info{
+      display: flex;
+      align-items: center;
+      gap: 10px;
+    }
+
+    .user-label{
+      font-size: 14px;
+      font-weight: 500;
+    }
+
+    .login-btn{
+      padding: 8px 12px;
+      cursor: pointer;
+    }
+  `
+  ]
 })
 export class LoginButton {
   protected auth = inject(AuthService);
--- frontend/mock-sims-app/src/app/pages/Dashboard.ts
+++ frontend/mock-sims-app/src/app/pages/Dashboard.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {AuthService} from '../services/auth';
 
 @Component({
   selector: "app-dashboard",
   standalone: true,
   templateUrl: './template/DashboardTemplate.html'
 })
-export class Dashboard{}
+export class Dashboard{
+  protected auth = inject(AuthService);
+}
