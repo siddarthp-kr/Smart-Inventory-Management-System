67f41a6456283b1adadcdeca660f15d24ef12ad4
Siddarth Padmanabhan <siddarth.padmanabhan@kroger.com>
2026-06-18 09:32:37 -0400

    testing little design

--- frontend/mock-sims-app/src/app/app.ts
+++ frontend/mock-sims-app/src/app/app.ts
@@ -1,45 +1,17 @@
-import {ChangeDetectorRef, Component, signal} from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { LoginPage} from './components/LoginPage';
-import {Dashboard} from './pages/Dashboard';
-import {LoginButton} from './components/LoginButton';
+import { Component, inject, signal } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { LoginButton } from './components/LoginButton';
+import { AuthService } from './services/auth';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLink, LoginPage, LoginButton],
-  template: `
-      <div class = "top-bar">
-        <h2>Smart Inventory System</h2>
-        <login-button />
-      </div>
-
-      <nav class = "nav-bar">
-        <a routerLink="/Dashboard">Dashboard</a>
-        <a routerLink="/OrderPage">Ordering</a>
-        <a routerLink="/AddItemPage">Add Item</a>
-        <a routerLink="/BohPage">BOH</a>
-        <a routerLink="/AlertsPage">Alerts</a>
-      </nav>
-
-      <div class = "content">
-        <router-outlet></router-outlet>
-      </div>
-
-
-  `,
-  //templateUrl: './app.html',
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, LoginButton],
+  templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('mock-sims-app');
-  currentUserEuid = ''
-  currentUserStoreNumber = ''
-  currentUserDivisionNumber = ''
+  protected readonly title = signal('frontend');
+  protected auth = inject(AuthService);
 
-  assignLoginCredentials(credentials: {euid: string, storeNumber: string, divisionNumber: string}){
-    this.currentUserEuid = credentials.euid
-    this.currentUserStoreNumber = credentials.storeNumber
-    this.currentUserDivisionNumber = credentials.divisionNumber
-  }
 }
