315c31e6e718b389dfb619ee22b6d0e030bc99fc
js58323 <jeev.sampath@kroger.com>
2026-06-29 09:33:14 -0400

    push back api service layer

--- mock-sims-server/mock-sims-service/src/main/java/mocksims/project/backend/service/PushBackExpirationServiceImpl.java
+++ mock-sims-server/mock-sims-service/src/main/java/mocksims/project/backend/service/PushBackExpirationServiceImpl.java
@@ -1,5 +1,6 @@
 package mocksims.project.backend.service;
 
+import mocksims.project.backend.api.domain.MarkdownRulesRecord;
 import mocksims.project.backend.api.domain.PushBackExpirationRequest;
 import mocksims.project.backend.controller.ProductsController;
 import mocksims.project.backend.repository.PushBackExpirationRepository;
@@ -7,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Service
 public class PushBackExpirationServiceImpl implements PushBackExpirationService {
@@ -22,7 +25,14 @@ public class PushBackExpirationServiceImpl implements PushBackExpirationService 
     @Override
     @Transactional
     public void pushBackExpirationDate(PushBackExpirationRequest pushBackExpirationRequest) {
+        String subcommodityNumber = pushBackExpirationRepository.getSubcommodityNumber(pushBackExpirationRequest.getAlertId());
+        MarkdownRulesRecord markdownRulesRecord = pushBackExpirationRepository.getMarkdownRules(subcommodityNumber);
+        LocalDate newMdDate = pushBackExpirationRequest.getNewExpirationDate().minusDays(markdownRulesRecord.getDaysBeforeExpToMd());
+        LocalDate newRfiDate = pushBackExpirationRequest.getNewExpirationDate().minusDays(markdownRulesRecord.getDaysBeforeExpToRfi());
 
+        pushBackExpirationRepository.insertNewAlert(pushBackExpirationRequest.getNewExpirationDate(), newRfiDate, newMdDate, pushBackExpirationRequest.getAlertId());
+
+        pushBackExpirationRepository.deactivateOldAlert(pushBackExpirationRequest.getAlertId());
         /*
         get subcommodity numbers from the alertid
         get new markdown rules from the subcommodity number
