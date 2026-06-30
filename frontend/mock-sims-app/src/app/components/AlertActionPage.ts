a0fa660882beb3f1fccfd1462b3ae49bdf927c82
js58323 <jeev.sampath@kroger.com>
2026-06-30 16:27:35 -0400

    Converted to american date format

--- frontend/mock-sims-app/src/app/components/AlertActionPage.ts
+++ frontend/mock-sims-app/src/app/components/AlertActionPage.ts
@@ -157,5 +157,14 @@ export class AlertActionPage implements OnInit {
     this.cd.detectChanges()
   }
 
+  convertDateFormat(dateValue: string){
+    const date = new Date(dateValue);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${month}-${day}-${year}`;
+  }
+
 
 }
